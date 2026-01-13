b3bf83d16a15dc6bd07cf7318ba56053baba5b95
Julia Vorobeva <vorobeva_ya1@itms.tech>
2026-01-13 13:12:54 +0300

    Initial Commit

---
+++ Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tracker
+//
+//  Created by Воробьева Юлия on 13.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
